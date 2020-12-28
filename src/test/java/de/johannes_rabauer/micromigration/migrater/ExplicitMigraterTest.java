19aae97a2f467449aaf8301762660c4d411c70bf
Johannes Rabauer <johannes@johannes-rabauer.de>
2020-12-28 14:31:25 +0100

    Implemented test for wrongly typed migration scripts

--- src/test/java/de/johannes_rabauer/micromigration/migrater/ExplicitMigraterTest.java
+++ src/test/java/de/johannes_rabauer/micromigration/migrater/ExplicitMigraterTest.java
@@ -2,8 +2,15 @@ package de.johannes_rabauer.micromigration.migrater;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.nio.file.Path;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import de.johannes_rabauer.micromigration.MigrationEmbeddedStorage;
+import de.johannes_rabauer.micromigration.MigrationEmbeddedStorageManager;
+import de.johannes_rabauer.micromigration.scripts.SimpleTypedMigrationScript;
 import de.johannes_rabauer.micromigration.testUtil.MicroMigrationScriptDummy;
 import de.johannes_rabauer.micromigration.version.MigrationVersion;
 
@@ -11,13 +18,15 @@ class ExplicitMigraterTest
 {
 	
 	@Test
-	void testGetSortedScripts_empty() {
+	void testGetSortedScripts_empty() 
+	{
 		final ExplicitMigrater migrater = new ExplicitMigrater();
 		assertEquals(0,migrater.getSortedScripts().size());
 	}	
 	
 	@Test
-	void testGetSortedScripts_sorted() {
+	void testGetSortedScripts_sorted() 
+	{
 		final ExplicitMigrater migrater = new ExplicitMigrater(
 			new MicroMigrationScriptDummy(new MigrationVersion(1)),
 			new MicroMigrationScriptDummy(new MigrationVersion(2))
@@ -27,13 +36,38 @@ class ExplicitMigraterTest
 	}	
 	
 	@Test
-	void testGetSortedScripts_unsorted() {
+	void testGetSortedScripts_unsorted() 
+	{
 		final ExplicitMigrater migrater = new ExplicitMigrater(
 			new MicroMigrationScriptDummy(new MigrationVersion(2)),
 			new MicroMigrationScriptDummy(new MigrationVersion(1))
 		);
 		assertEquals(1, migrater.getSortedScripts().first().getTargetVersion().getVersions()[0]);
 		assertEquals(2, migrater.getSortedScripts().last().getTargetVersion().getVersions()[0]);
+	}	
+	
+	@Test
+	void testWrongTypedVersionedScript(@TempDir Path storageFolder) 
+	{		
+		try(final MigrationEmbeddedStorageManager storageManager = MigrationEmbeddedStorage.start(storageFolder, new ExplicitMigrater()))
+		{
+			storageManager.setRoot("SomeString");
+			storageManager.storeRoot();
+		}
+		final ExplicitMigrater migrater = new ExplicitMigrater(
+			new SimpleTypedMigrationScript<Double>(
+				new MigrationVersion(1),
+				(context) -> {
+					context.getStorageManager().setRoot(context.getMigratingObject() + 1);
+				}
+			)
+		);
+		Assertions.assertThrows(ClassCastException.class, () -> 
+			{
+				MigrationEmbeddedStorage.start(storageFolder, migrater);
+			}
+		);
+
 	}
 
 }
