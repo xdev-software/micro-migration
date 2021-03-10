4c336bae3d2ee8b301019dba4dac5d6ad7ce671b
Johannes Rabauer <johannes@johannes-rabauer.de>
2021-03-10 22:33:50 +0100

    New Tests and Checks for same versions registered in one migrater

--- src/main/java/de/johannes_rabauer/micromigration/migrater/ExplicitMigrater.java
+++ src/main/java/de/johannes_rabauer/micromigration/migrater/ExplicitMigrater.java
@@ -3,6 +3,7 @@ package de.johannes_rabauer.micromigration.migrater;
 import java.util.TreeSet;
 
 import de.johannes_rabauer.micromigration.scripts.MigrationScript;
+import de.johannes_rabauer.micromigration.version.MigrationVersion;
 
 /**
  * Contains all the available scripts to migrate the datastore to a certain version.
@@ -17,12 +18,26 @@ public class ExplicitMigrater extends AbstractMigrater
 	private final TreeSet<MigrationScript<?>> sortedScripts = new TreeSet<>(MigrationScript.COMPARATOR);
 	
 	/**
-	 * @param scripts are all the scripts that are executed, if the current version is lower than this of the script
+	 * @param scripts are all the scripts that are executed, if the current version is lower than this of the script<br>
+	 * Versions of the scripts must be unique. That means that no version is allowed multiple times in the migrater.
+	 * @throws VersionAlreadyRegisteredException 
 	 */
-	public ExplicitMigrater(MigrationScript<?> ...scripts)
+	public ExplicitMigrater(MigrationScript<?> ...scripts) throws VersionAlreadyRegisteredException
 	{
 		for (MigrationScript<?> script : scripts) 
 		{
+			for (MigrationScript<?> alreadyRegisteredScript : this.sortedScripts) 
+			{
+				if(MigrationVersion.COMPARATOR.compare(alreadyRegisteredScript.getTargetVersion(), script.getTargetVersion()) == 0)
+				{
+					//Two scripts with the same version are not allowed to get registered.
+					throw new VersionAlreadyRegisteredException(
+							alreadyRegisteredScript.getTargetVersion(),
+							alreadyRegisteredScript,
+							script
+					);
+				}
+			}
 			this.sortedScripts.add(script);
 		}
 	}
---
+++ src/test/java/de/johannes_rabauer/micromigration/integration/MultipleScriptsTest.java
@@ -0,0 +1,75 @@
+package de.johannes_rabauer.micromigration.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import de.johannes_rabauer.micromigration.MigrationEmbeddedStorage;
+import de.johannes_rabauer.micromigration.MigrationEmbeddedStorageManager;
+import de.johannes_rabauer.micromigration.migrater.ExplicitMigrater;
+import de.johannes_rabauer.micromigration.migrater.VersionAlreadyRegisteredException;
+import de.johannes_rabauer.micromigration.scripts.MigrationScript;
+import de.johannes_rabauer.micromigration.scripts.SimpleTypedMigrationScript;
+import de.johannes_rabauer.micromigration.version.MigrationVersion;
+
+class MultipleScriptsTest 
+{
+	@Test
+	void testMigrationWithTwoScriptsAtOnce_MigrationEmbeddedStorageManager(@TempDir Path storageFolder)
+	{
+		final MigrationScript<Integer> firstScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(1), 
+				(context) -> context.getStorageManager().setRoot(1)
+		);
+		final MigrationScript<Integer> secondScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(2), 
+				(context) -> context.getStorageManager().setRoot(2)
+		);
+		final ExplicitMigrater migrater = new ExplicitMigrater(firstScript, secondScript);
+		try(final MigrationEmbeddedStorageManager migrationStorageManager = MigrationEmbeddedStorage.start(storageFolder, migrater))
+		{
+			assertEquals(2, migrationStorageManager.root());
+			assertEquals(new MigrationVersion(2), migrationStorageManager.getCurrentVersion());
+		}
+	}
+	
+	@Test
+	void testMigrationWithTwoScriptsWithSameVersion(@TempDir Path storageFolder)
+	{
+		final MigrationScript<Integer> firstScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(1), 
+				(context) -> context.getStorageManager().setRoot(1)
+		);
+		final MigrationScript<Integer> secondScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(1), 
+				(context) -> context.getStorageManager().setRoot(2)
+		);
+		Assertions.assertThrows(VersionAlreadyRegisteredException.class, () -> 
+			new ExplicitMigrater(firstScript, secondScript)
+		);
+	}
+	
+	@Test
+	void testMigrationWithThreeScriptsWithSameVersion(@TempDir Path storageFolder)
+	{
+		final MigrationScript<Integer> firstScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(1), 
+				(context) -> context.getStorageManager().setRoot(1)
+		);
+		final MigrationScript<Integer> secondScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(2), 
+				(context) -> context.getStorageManager().setRoot(2)
+		);
+		final MigrationScript<Integer> thirdScript = new SimpleTypedMigrationScript<>(
+				new MigrationVersion(1), 
+				(context) -> context.getStorageManager().setRoot(3)
+		);
+		Assertions.assertThrows(VersionAlreadyRegisteredException.class, () -> 
+			new ExplicitMigrater(firstScript, secondScript, thirdScript)
+		);
+	}
+}
